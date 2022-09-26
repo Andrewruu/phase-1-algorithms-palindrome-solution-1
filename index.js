e51dee9ce4596088d4623bb683b4b44ea8bde176
Andrew Wang <Andrewruujin@gmail.com>
2022-09-26 15:01:53 -0400

    commit tree 7a70d18a5f9ab2227c4b9eb7ccc0e1b309eb7eda

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let specChar = /[\W_]/g
+  let lowerWord = word.toLowerCase().replace(specChar, '')
+  let reverseWord = lowerWord.split('').reverse().join('')
+  return reverseWord === lowerWord
 }
 
 /* 
-  Add your pseudocode here
+reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
-  Add written explanation of your solution here
+    /[\W_]/g;
+  looked up how to search of away to remove special chars
+  
+  [ and ] are the start and end of a character set.
+  \W means "non-word", as opposed to \w which will match a word.
+  _ is the "_" character.
+  / mark the beginning and end of a regular expression.
+  g means it's a global search. 
+
+  I then set the word to be lower case so it will not be case sensitive and replace special char to remove them
+  Next I reversed the words after spliting it and then i rejoined them
+  last i check if the reviesed word is equal to the lower case word.
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,6 +35,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
